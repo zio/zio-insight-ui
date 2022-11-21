e310bf8abc76ca3216fdf32c6ecb8a3681d789f4
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-21 20:29:27 +0000

    add visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react"
 import path from "path"
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
 import tsConfigPaths from "vite-tsconfig-paths"
@@ -26,6 +27,7 @@ export default defineConfig({
       }
     }),
     react(),
-    tsConfigPaths()
+    tsConfigPaths(),
+    visualizer()
   ]
 })
