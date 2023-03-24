fcf25eaa0eec7ab7d9cfd7a9ae51a2d7bbd5bbac
Andreas Gies <andreas@wayofquality.de>
2023-03-24 17:51:33 +0100

    Switch to Effect provided Logging Service

--- test/core/services/idsvc.test.ts
+++ test/core/services/idsvc.test.ts
@@ -2,10 +2,9 @@ import * as RT from "@effect/io/Runtime"
 
 import * as AL from "@core/AppLayer"
 import * as IdSvc from "@core/services/IdGenerator"
-import * as Log from "@core/services/Logger"
 
 describe("IdSvc", () => {
-  const testRt = AL.unsafeMakeRuntime(AL.appLayerStatic(Log.All)).runtime
+  const testRt = AL.unsafeMakeRuntime(AL.appLayerStatic).runtime
 
   it("should generate an id", async () => {
     const res = await RT.runPromiseExit(testRt)(IdSvc.nextId("app"))
--- test/hub.test.ts
+++ test/hub.test.ts
@@ -4,27 +4,31 @@ import { pipe } from "@effect/data/Function"
 import * as T from "@effect/io/Effect"
 import * as F from "@effect/io/Fiber"
 import * as H from "@effect/io/Hub"
+import * as Sch from "@effect/io/Schedule"
 import * as S from "@effect/stream/Stream"
 
 describe("PlayHub", () => {
-  it("should consume messages from a topic", async () => {
+  it("should consume messages from a hub", async () => {
+    const n = 2
     const res = await T.runPromise(
       T.gen(function* ($) {
         const hub = yield* $(H.unbounded<string>())
 
         const stream = pipe(
           S.fromHub(hub),
-          S.take(1),
+          S.take(n),
           S.mapEffect((s) => T.log(`ooo: ${s}`))
         )
 
         const f1 = yield* $(pipe(S.runCollect(stream), T.fork))
-        yield* $(T.delay(D.millis(10))(hub.publish("hello")))
+        yield* $(
+          pipe(T.schedule(Sch.spaced(D.millis(100)))(hub.publish("hello")), T.fork)
+        )
 
         return yield* $(F.join(f1))
       })
     )
 
-    expect(C.size(res)).toEqual(1)
+    expect(C.size(res)).toEqual(n)
   })
 })
