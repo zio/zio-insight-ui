eef268ebbee2d479db145d1eb942716859ade580
Andreas Gies <andreas@wayofquality.de>
2023-03-31 20:20:38 +0200

    Adding some comments

--- src/components/d3/SvgPanel.tsx
+++ src/components/d3/SvgPanel.tsx
@@ -1,14 +1,19 @@
+// Import React and D3Utils
 import * as React from "react"
 
 import * as D3Utils from "./Utils"
 
+// Create SVGDimensions context to store the dimensions of the SVG element
 export const SVGDimensions = React.createContext<D3Utils.Dimensions>(
   D3Utils.emptyDimensions
 )
 
+// SVGPanel component that renders a div with an SVG element inside
 export const SVGPanel: React.FC<React.PropsWithChildren<{}>> = (props) => {
+  // Use useDimensions hook to get the dimensions of the div element
   const [ref, dimensions] = D3Utils.useDimensions<HTMLDivElement>()
 
+  // Create an SVG element using the dimensions
   const element = React.useMemo(() => {
     const svgContent = (
       <SVGDimensions.Provider value={dimensions}>
@@ -23,6 +28,7 @@ export const SVGPanel: React.FC<React.PropsWithChildren<{}>> = (props) => {
     return React.createElement("svg", { width: "100%", height: "100%" }, svgContent)
   }, [dimensions])
 
+  // Render the div with the SVG element
   return (
     <div ref={ref} className="grow flex relative">
       {element}
--- src/components/d3/Utils.ts
+++ src/components/d3/Utils.ts
@@ -1,11 +1,14 @@
+// Imports the library for ResizeObserver
 import { ResizeObserver } from "@juggle/resize-observer"
 import * as React from "react"
 
+// Default margin values
 const topDefault = 40
 const rightDefault = 30
 const bottomDefault = 40
 const leftDefault = 75
 
+// Interface for margins
 export interface Margins {
   top?: number
   right?: number
@@ -13,12 +16,14 @@ export interface Margins {
   left?: number
 }
 
+// Interface for Dimensions
 export interface Dimensions {
   height: number
   width: number
   margins: Margins
 }
 
+// Function to return bounded dimensions
 export function boundedDimensions(d: Dimensions): [number, number] {
   return [
     Math.max(d.width - (d.margins.left || 0) - (d.margins.right || 0), 0),
@@ -26,6 +31,7 @@ export function boundedDimensions(d: Dimensions): [number, number] {
   ]
 }
 
+// Function to combine chart dimensions
 const combineChartDimensions = (d: Dimensions) => {
   return {
     width: d.width,
@@ -39,12 +45,14 @@ const combineChartDimensions = (d: Dimensions) => {
   } as Dimensions
 }
 
+// Empty dimensions
 export const emptyDimensions = combineChartDimensions({
   width: 0,
   height: 0,
   margins: {},
 })
 
+// Hook to use dimensions
 export function useDimensions<T extends Element>(
   w?: number,
   h?: number,
@@ -52,20 +60,25 @@ export function useDimensions<T extends Element>(
 ): [React.MutableRefObject<T | null>, Dimensions] {
   const ref = React.useRef<T>(null)
 
+  // State to store current dimensions
   const [currentDms, changeDms] = React.useState(emptyDimensions)
 
+  // Effect to observe changes in element
   React.useEffect(() => {
     const element = ref.current
 
+    // If width and height are passed in, set dimensions accordingly
     if (w && h) {
       changeDms(combineChartDimensions({ width: w, height: h, margins }))
       return
     } else {
+      // Create a new ResizeObserver
       const resizeObserver = new ResizeObserver(
         (entries: Array<ResizeObserverEntry>) => {
           if (entries.length == 0) return
           const entry = entries[0]
 
+          // If width or height has changed, update dimensions
           if (
             currentDms.width != entry.contentRect.width ||
             currentDms.height != entry.contentRect.height
@@ -81,10 +94,12 @@ export function useDimensions<T extends Element>(
         }
       )
 
+      // Observe the element
       if (element) {
         resizeObserver.observe(element, { box: "content-box" })
       }
 
+      // Disconnect the observer
       return () => resizeObserver.disconnect()
     }
   }, [ref, w, h, margins])
